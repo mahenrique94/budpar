25203981c490419f23d97fdeb519791e62203f64
Matheus <matheushcastiglioni@gmail.com>
2016-06-15 09:26:57 -0300

    Adicionado usuário para GI testar o sistema

--- DB.sql
+++ DB.sql
@@ -8,6 +8,14 @@ db.usuario.insert({
 });
 
 db.usuario.insert({
+   nome : "GI",
+   usuario : "GI",
+   senha : "123456",
+   datacreate : new Date(),
+   dataupdate : new Date() 
+});
+
+db.usuario.insert({
    nome : "ADS UNOPAR 2016",
    usuario : "ADSUNOPAR",
    senha : "UNOPAR2016",
