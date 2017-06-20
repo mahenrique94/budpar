6d825b991a51a52fea2fa22a8e86019ef39564c6
Matheus Castiglioni <mahenrique94@users.noreply.github.com>
2017-06-19 21:09:53 -0300

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,24 +1,8 @@
 -- USUARIO
 db.usuario.insert({
-   nome : "MATHEUS HENRIQUE",
-   usuario : "MAHENRIQUE94",
-   senha : "MAHMAH10",
-   datacreate : new Date(),
-   dataupdate : new Date() 
-});
-
-db.usuario.insert({
-   nome : "GI",
-   usuario : "GI",
-   senha : "123456",
-   datacreate : new Date(),
-   dataupdate : new Date() 
-});
-
-db.usuario.insert({
-   nome : "ADS UNOPAR 2016",
-   usuario : "ADSUNOPAR",
-   senha : "UNOPAR2016",
+   nome : "ROOT",
+   usuario : "ROOT",
+   senha : "ROOT",
    datacreate : new Date(),
    dataupdate : new Date() 
 });
